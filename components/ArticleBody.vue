72fb1749ca341e6de3d9a63e8bf7c948d8817149
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-05-04 20:36:43 +0900

    add article index & detail

---
+++ components/ArticleBody.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="article">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content has-text-centered">
+          <h1 class="title article-title ">記事タイトル①</h1>
+          <p class="subtitle is-6 article-subtitle has-text-right">
+            2018/04/01
+          </p>
+        </div>
+      </div>
+      <div class="content article-body">
+        <!-- Markdownから取得した記事の本文をここに記載する -->
+        <p>Non arcu risus quis varius quam quisque. Dictum varius duis at consectetur lorem. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. </p>
+        <p>Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Accumsan lacus vel facilisis volutpat. Non sodales neque sodales ut etiam. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.</p>
+        <h3 class="has-text-centered">Feugiat sed lectus vestibulum mattis.</h3>
+        <p> Molestie ac feugiat sed lectus vestibulum. Feugiat sed lectus vestibulum mattis. Volutpat diam ut venenatis tellus in metus vulputate. Feugiat in fermentum posuere urna nec. Pharetra convallis posuere morbi leo urna molestie at. Accumsan lacus vel facilisis volutpat est velit egestas. Fermentum leo vel orci porta. Faucibus interdum posuere lorem ipsum.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.media-center {
+  display: block;
+  margin-bottom: 1rem;
+}
+.content p {
+    line-height: 1.9;
+    margin: 15px 0;
+}
+.media-content {
+  margin-top: 3rem;
+}
+.article, .promo-block {
+  margin-top: 6rem;
+}
+div.column.is-8:first-child {
+  padding-top: 0;
+  margin-top: 0;
+}
+.article-title {
+  font-size: 2rem;
+  font-weight: lighter;
+  line-height: 2;
+}
+.article-subtitle {
+  color: #909AA0;
+  margin-bottom: 3rem;
+}
+.article-body {
+  line-height: 1.4;
+  margin: 0 6rem;
+}
+.promo-block .container {
+  margin: 1rem 5rem;
+}
+</style>
---
+++ pages/post/_title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <article-main />
+    <p>title: {{title}}</p>
+  </div>
+</template>
+
+<script>
+import ArticleMain from '~/components/ArticleMain'
+
+export default {
+  asyncData ({ params }) {
+    const { title } = params
+    return {
+      title
+    }
+  },
+  components: { ArticleMain }
+}
+</script>
