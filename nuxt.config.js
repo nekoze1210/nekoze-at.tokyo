1da02db72579a232febc1f12f819a01f761bb6dc
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-05-04 22:24:29 +0900

    add Nuxtent

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,23 +2,23 @@ module.exports = {
   /*
   ** Headers of the page
   */
- head: {
-   title: 'Do What Works',
-   meta: [
-     { charset: 'utf-8' },
-     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-     { hid: 'description', name: 'description', content: '@cuten77' }
+  head: {
+    title: 'Do What Works',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '@cuten77' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
   ** Customize the progress bar color
   */
- loading: { color: '#3B8070' },
- /*
+  loading: { color: '#3B8070' },
+  /*
  ** Build configuration
  */
-  modules: ['@nuxtjs/font-awesome'],
+  modules: ['@nuxtjs/font-awesome', 'nuxtent'],
   css: ['buefy/lib/buefy.css'],
   build: {
     /*
