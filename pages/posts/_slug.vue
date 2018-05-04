41fc491c1910631e84ced36097422dcd7f12feae
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-05-05 01:09:47 +0900

    remove nuxtent transactions from posts/:slug

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <article-main />
-    <nuxtent-body :body="content.title"/>
-    <!-- <p>slug: {{slug}}</p> -->
   </div>
 </template>
 
@@ -10,14 +8,6 @@
 import ArticleMain from '~/components/ArticleMain'
 
 export default {
-  async asyncData ({ app, route, payload }) {
-    console.log(app)
-    console.log(route)
-    console.log(payload)
-    return {
-      content: await app.$content('/').get(route.path)
-    }
-  },
   components: { ArticleMain }
 }
 </script>
