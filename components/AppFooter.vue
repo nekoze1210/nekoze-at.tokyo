cc1bb76e9ac2f4dcd685ca6be611cc8fd2ce7c17
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-05-04 20:35:55 +0900

    modify header & footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          <strong>do-what-works</strong> by <a href="https://jgthms.com">Daiki Nagaoka</a>.
+          <strong>Do What Works</strong> by <a href="https://jgthms.com">Daiki Nagaoka</a>. The source code is licensed
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
           is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,9 @@
 <template>
-  <header class='navbar is-transparent is-fixed-top' role='navigation'>
-    <div class='navbar-brand'>
-      <router-link to='/' class='navbar-item'>Do What Works</router-link>
-      <a role="button" class="navbar-burger" data-target="side-menu" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div id="side-menu" class="navbar-menu">
+  <nav class='navbar is-fixed-top' role='navigation'>
+    <app-logo></app-logo>
+    <div id="sideMenu" class="navbar-menu">
       <div class="navbar-end">
-        <div class="navbar-item ">
+        <div class="navbar-item">
           <p class="control has-icons-left">
             <input class="input" type="text" placeholder="Search">
             <span class="icon is-left">
@@ -18,7 +11,27 @@
             </span>
           </p>
         </div>
+        <div class="navbar-item">
+          <router-link to="/sitemap"></router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link to="#"><i class="fa fa-rss"></i></router-link>
+        </div>
+        <div class="navbar-item">
+          <a href="https://github.com/nekoze1210" target="_blank"><i class="fa fa-github"></i></a>
+        </div>
       </div>
     </div>
-  </header>
+  </nav>
 </template>
+
+<script>
+  import AppLogo from '~/components/AppLogo'
+
+  export default {
+    components: { AppLogo }
+  }
+</script>
