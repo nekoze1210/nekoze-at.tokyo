7edb7a525ae5daf54f38e05d49360427e4626db0
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-05-12 16:43:59 +0900

    refactor applogo

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class='navbar-brand'>
-    <nuxt-link to='/' class='navbar-item navbar-item-logo'><span class="has-text-success">D</span>o <span class="has-text-success">W</span>hat <span class="has-text-success">W</span>orks</nuxt-link>
-    <a type="button" class="navbar-burger burger" data-target="sideMenu">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+  <nuxt-link to='/' class='navbar-item navbar-item-logo'><span class="has-text-success">D</span>o <span class="has-text-success">W</span>hat <span class="has-text-success">W</span>orks</nuxt-link>
 </template>
 
 <style scoped>
