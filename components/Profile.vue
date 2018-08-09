2414a15d180718ed2b6b22f3a3caa84e637eb4b2
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-08-09 22:13:54 +0900

    duplicated with profile component

---
+++ components/Profile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="profile">
+    <h2 class="profile__heading">Profile</h2>
+    <hr class="profile__ruler">
+    <figure class="image profile__avatar">
+      <img src="~/assets/profile.jpg" alt="@cuten77" class="is-rounded">
+    </figure>
+    <h3 class="is-3 profile__myname">Daiki Nagaoka</h3>
+    <p>Web Engineer</p>
+    <!-- <button>Profile</button> -->
+  </div>
+</template>
+
+<style lang="scss">
+.profile {
+  margin-top: 3.5em;
+
+  &__heading {
+    width: 177px;
+    height: 68px;
+    font-family: Avenir;
+    font-size: 50px;
+    font-weight: 900;
+    font-style: oblique;
+    font-stretch: normal;
+    line-height: 1.36;
+    letter-spacing: normal;
+    text-align: left;
+    color: #434040;
+  }
+
+  &__ruler {
+    border: solid 1px #707070;
+  }
+
+  &__avatar {
+    width: 204px;
+    height: 214px;
+    margin: 0 auto;
+    img {
+      box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.16);
+      border: solid 1px #707070;
+    }
+  }
+  &__myname {
+    font-family: Avenir;
+    font-size: 25px;
+    font-weight: 500;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.36;
+    letter-spacing: normal;
+    text-align: left;
+    color: #000000;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -22,16 +22,7 @@
       </div>
     </section>
     <section class="column">
-      <div class="profile">
-        <h2 class="profile__heading">Profile</h2>
-        <hr class="profile__ruler">
-        <figure class="image profile__avatar">
-          <img src="~/assets/profile.jpg" alt="@cuten77" class="is-rounded">
-        </figure>
-        <h3 class="is-3 profile__myname">Daiki Nagaoka</h3>
-        <p>Web Engineer</p>
-        <!-- <button>Profile</button> -->
-      </div>
+      <profile></profile>
       <div class="sponsor">
         <h2 class="sponsor__heading">Ads</h2>
         <hr class="sponsor__ruler">
@@ -49,6 +40,7 @@
 
 <script>
 import { createClient } from '~/plugins/contentful.js'
+import profile from '~/components/Profile.vue'
 
 const client = createClient()
 export default {
@@ -173,49 +165,6 @@ export default {
     .card {
       margin-right: 15%;
     }
-  }
-}
-
-.profile {
-  margin-top: 3.5em;
-
-  &__heading {
-    width: 177px;
-    height: 68px;
-    font-family: Avenir;
-    font-size: 50px;
-    font-weight: 900;
-    font-style: oblique;
-    font-stretch: normal;
-    line-height: 1.36;
-    letter-spacing: normal;
-    text-align: left;
-    color: #434040;
-  }
-
-  &__ruler {
-    border: solid 1px #707070;
-  }
-
-  &__avatar {
-    width: 204px;
-    height: 214px;
-    margin: 0 auto;
-    img {
-      box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.16);
-      border: solid 1px #707070;
-    }
-  }
-  &__myname {
-    font-family: Avenir;
-    font-size: 25px;
-    font-weight: 500;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.36;
-    letter-spacing: normal;
-    text-align: left;
-    color: #000000;
   }
 }
 
