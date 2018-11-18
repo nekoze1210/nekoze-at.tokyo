4fa78a231a2827f7bcc29dcf9891d7eb31be5365
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-11-18 16:08:04 +0900

    modify root page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <div class="wrap"><profile /></div>
+    <!-- <vue-particles
+      color="#247e55"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#247e55"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="4"
+      :hoverEffect="false"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    ></vue-particles> -->
+  </div>
 </template>
 
 <script>
-import { createClient } from '~/plugins/contentful.js'
+import contentful from '~/plugins/contentful.js'
+// import VueParticle from '~/plugins/vue-particle.js'
+import Profile from '~/components/Profile.vue'
 
-const client = createClient()
+const client = contentful.createClient()
 export default {
   fetch({ store }) {
     store.commit('resetMenu')
@@ -33,9 +54,21 @@ export default {
       })
       .catch(console.error)
   },
-  components: { profile }
+  components: { Profile }
 }
 </script>
 
 <style lang="scss" scoped>
+.wrap {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  height: 100vh;
+}
 </style>
