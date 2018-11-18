a0379d5e2b36e838a3b32e4d4e8cdcf8e2da59a8
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-11-18 16:06:33 +0900

    modify profile section

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,62 +1,68 @@
 <template>
-  <div class="profile">
-    <h2 class="profile__heading">Profile</h2>
-    <hr class="profile__ruler">
-    <figure class="image profile__avatar">
-      <img src="~/assets/profile.jpg" alt="@cuten77" class="is-rounded">
-    </figure>
-    <h3 class="is-3 profile__myname">Daiki Nagaoka</h3>
-    <p>Web Engineer</p>
-    <nuxt-link to="/profile" class="button is-fullwidth">more...</nuxt-link>
-  </div>
+  <section class="container">
+    <div class="columns is-centered">
+      <div class="column is-two-thirds">
+        <h3 class="is-3 profile__myname">{{ myName }} </h3>
+        <h4 class="profile__myjob">{{ myJob }}</h4>
+      </div>
+      <div class="column">
+        <figure class="image profile__avatar">
+          <img src="~/assets/profile.jpg" alt="@cuten77" class="is-rounded">
+        </figure>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  data() {
+    return {
+      myName: 'Daiki Nagaoka',
+      myJob: 'Web Developer'
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .profile {
-  margin-top: 3.5em;
-
-  &__heading {
-    width: 177px;
-    font-family: Avenir;
-    font-size: 50px;
-    font-weight: 900;
-    font-style: oblique;
-    font-stretch: normal;
-    line-height: 1.36;
-    letter-spacing: normal;
-    text-align: left;
-    color: #434040;
-  }
-
-  &__ruler {
-    border: solid 1px #707070;
-  }
-
   &__avatar {
     width: 204px;
     height: 214px;
-    margin: 0 auto;
     img {
       box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.16);
       border: solid 1px #707070;
     }
   }
   &__myname {
-    font-family: Avenir;
-    font-size: 25px;
-    font-weight: 500;
+    font-family: YuGo;
+    font-size: 30px;
+    font-weight: bold;
     font-style: normal;
     font-stretch: normal;
-    line-height: 1.36;
-    letter-spacing: normal;
+    line-height: 1.7;
+    letter-spacing: 3px;
     text-align: left;
-    color: #000000;
+    color: #444444;
+  }
+  &__myjob {
+    font-family: Avenir;
+    font-size: 20px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.35;
+    letter-spacing: 2px;
+    text-align: left;
+    color: #707070;
   }
 }
 
