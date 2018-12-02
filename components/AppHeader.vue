c9d3b59afb89dc7ad4a8879b31f292e07c3450f3
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-02 15:38:30 +0900

    uncomment burger menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,8 +1,14 @@
 <template>
-  <nav class='navbar is-transparent' role='navigation'>
-    <div class='navbar-brand'>
-      <app-logo />
-      <a type="button" class="navbar-burger burger" data-target="sideMenu" @click="$store.commit('toggleMenu')" :class="{ 'is-active' : $store.state.isMenuActive }">
+  <nav class="navbar is-transparent" role="navigation">
+    <div class="navbar-brand">
+      <app-logo/>
+      <a
+        type="button"
+        class="navbar-burger burger"
+        data-target="sideMenu"
+        @click="$store.commit('toggleMenu')"
+        :class="{ 'is-active' : $store.state.isMenuActive }"
+      >
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -13,22 +19,23 @@
         <nuxt-link to="/#about" class="navbar-item is-tab" exact>About</nuxt-link>
         <nuxt-link to="/#skills" class="navbar-item is-tab" exact>Skills & Works</nuxt-link>
         <nuxt-link to="/#articles" class="navbar-item is-tab" exact>Articles</nuxt-link>
+        <nuxt-link to="/#contacts" class="navbar-item is-tab" exact>Contacts</nuxt-link>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo'
+import AppLogo from "~/components/AppLogo";
 
 export default {
   data() {
     return {
       isActive: false
-    }
+    };
   },
   components: { AppLogo }
-}
+};
 </script>
 
 <style lang="scss" scoped>
