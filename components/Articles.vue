131a184f4ba173209d05d6a84f531d95a6e750ff
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-02 15:42:34 +0900

    create articles contacts vue component

---
+++ components/Articles.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="column">
+    <div class="container has-text-centered">
+      <h3>Articles</h3>
+      <h4>独学・仕事で学んだ知見など</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  font-family: Avenir;
+  font-size: 25px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.36;
+  letter-spacing: 2.5px;
+  text-align: center;
+  color: #707070;
+  margin-top: 60px;
+}
+
+h4 {
+  font-family: Avenir;
+  font-size: 15px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: 1.5px;
+  text-align: center;
+  color: #707070;
+  margin-top: 11.1px;
+}
+</style>
---
+++ components/Contacts.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="column">
+    <div class="container has-text-centered">
+      <h3>Contacts</h3>
+      <h4>お仕事相談したい方・会いたい方、ぜひ</h4>
+      <script src="https://sdk.form.run/js/v2/embed.js"></script>
+      <div class="form-run">
+        <div class="formrun-embed" data-formrun-form="@nekoze-at-tokyo"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  font-family: Avenir;
+  font-size: 25px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.36;
+  letter-spacing: 2.5px;
+  text-align: center;
+  color: #707070;
+  margin-top: 60px;
+}
+
+h4 {
+  font-family: Avenir;
+  font-size: 15px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: 1.5px;
+  text-align: center;
+  color: #707070;
+  margin-top: 11.1px;
+}
+
+.form-run {
+  border: 1px solid #4444;
+  margin-top: 30px;
+}
+</style>
