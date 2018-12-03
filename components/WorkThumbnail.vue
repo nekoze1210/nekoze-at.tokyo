7edc8a1f4c6113f40a2f617569fcbac15a2f1180
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-04 00:35:30 +0900

    delete debug code

--- components/WorkThumbnail.vue
+++ components/WorkThumbnail.vue
@@ -5,7 +5,6 @@
         <figure class="image is-4by3" @click="clickWork(work)">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
         </figure>
-        <p>{{ work }}</p>
       </div>
     </div>
   </div>
