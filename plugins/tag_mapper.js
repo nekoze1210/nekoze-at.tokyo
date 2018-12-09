65cb9d1d44ba450f3ca3e3585860af0cb96b9c94
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-09 17:47:16 +0900

    add `var`

--- plugins/tag_mapper.js
+++ plugins/tag_mapper.js
@@ -1,6 +1,6 @@
 export default {
   setTagPages(tags) {
-    filteredTags = []
+    var filteredTags = []
     tags.forEach(function(articleTag) {
       articleTag.forEach(function(mappedTag) {
         filteredTags.push(mappedTag)
