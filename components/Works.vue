109a0e27de61f83bbe467a02afe65fec6e84f0db
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-09 22:10:30 +0900

    add more works

--- components/Works.vue
+++ components/Works.vue
@@ -36,6 +36,28 @@ export default {
           period: "1ヶ月",
           detail: "",
           tag: "Vue.js"
+        },
+        {
+          name: "今年の漢字博士",
+          url:
+            "https://assistant.google.com/services/a/uid/000000e75d0ebfdf?hl=ja",
+          technology: "Javascript(ES6)/dialogflow/Google Assistant",
+          thumbnail: require("~/assets/work_thumbnails/kanji.png"),
+          period: "2週間",
+          detail:
+            "GoogleAssistantで歴代の「今年の漢字」を教えてくれるアプリを開発。",
+          tag: "Javascript"
+        },
+        {
+          name: "hankemory",
+          url:
+            "https://play.google.com/store/apps/details?id=io.nekoze.hankemory&hl=ja",
+          technology: "Vue.js/Cordova/Android SDK",
+          thumbnail: require("~/assets/work_thumbnails/hankemory.png"),
+          period: "2週間",
+          detail:
+            "映画館で発券したチケットの半券を思い出として記録できるアプリ。画面の状態遷移・機能はすべてVue.js製。Cordovaを利用し、ハイブリットアプリとして現在GooglePlayストアに公開中",
+          tag: "Javascript"
         }
       ]
     };
