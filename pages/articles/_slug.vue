7ef313f37462378ae4fdda51cdd7278430652836
Daiki Nagaoka <neko.z.n1210@gmail.com>
2018-12-30 11:31:05 +0900

    fix article slug designs

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,43 +1,53 @@
 <template>
-  <div class="container">
-    <div class="post">
-      <div class="column is-paddingless">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <h1 class="title article-title has-text-left">{{ currentPost.fields.title }}</h1>
-              <p
-                class="subtitle is-6 article-subtitle has-text-right"
-              >{{ ( new Date(currentPost.fields.published)).toDateString() }}</p>
-              <div class="tags">
-                <span v-for="tag in currentPost.fields.tags" :key="tag" class="tag">
-                  <nuxt-link :to="{ name: 'tags-tag', params: { tag: tag }}">#{{ tag }}</nuxt-link>
-                </span>
+  <div>
+    <div class="hero is-large">
+      <div
+        class="hero-body"
+        :style="{'background-image': 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(' + currentPost.fields.hero_image.fields.file.url + ')' }"
+      >
+        <div class="hero-image"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="post">
+        <div class="column is-paddingless">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <h1 class="title article-title has-text-left">{{ currentPost.fields.title }}</h1>
+                <p
+                  class="subtitle is-6 article-subtitle has-text-right"
+                >{{ ( new Date(currentPost.fields.published)).toDateString() }}</p>
+                <div class="tags">
+                  <span v-for="tag in currentPost.fields.tags" :key="tag" class="tag">
+                    <nuxt-link :to="{ name: 'tags-tag', params: { tag: tag }}">#{{ tag }}</nuxt-link>
+                  </span>
+                </div>
               </div>
             </div>
+            <div class="article-body">
+              <vue-markdown class="content">{{ currentPost.fields.body }}</vue-markdown>
+            </div>
+            <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+              <nuxt-link
+                v-if="prevPost"
+                class="pagination-previous"
+                :to="prevPost.fields.slug"
+              >&laquo; {{ prevPost.fields.title | truncate(20, '...') }}</nuxt-link>
+              <nuxt-link
+                v-if="nextPost"
+                class="pagination-next"
+                :to="nextPost.fields.slug"
+              >{{ nextPost.fields.title | truncate(20, '...') }} &raquo;</nuxt-link>
+            </nav>
+            <no-ssr>
+              <adsbygoogle
+                :ad-slot="'8702010588'"
+                :ad-format="'fluid'"
+                :ad-layout-key="'-fb+5w+4e-db+86'"
+              />
+            </no-ssr>
           </div>
-          <div class="article-body">
-            <vue-markdown class="content">{{ currentPost.fields.body }}</vue-markdown>
-          </div>
-          <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-            <nuxt-link
-              v-if="prevPost"
-              class="pagination-previous"
-              :to="prevPost.fields.slug"
-            >&laquo; {{ prevPost.fields.title }}</nuxt-link>
-            <nuxt-link
-              v-if="nextPost"
-              class="pagination-next"
-              :to="nextPost.fields.slug"
-            >{{ nextPost.fields.title }} &raquo;</nuxt-link>
-          </nav>
-          <no-ssr>
-            <adsbygoogle
-              :ad-slot="'8702010588'"
-              :ad-format="'fluid'"
-              :ad-layout-key="'-fb+5w+4e-db+86'"
-            />
-          </no-ssr>
         </div>
       </div>
     </div>
@@ -117,6 +127,11 @@ export default {
         return this.allPosts[this.dateOrder + 1];
       }
     }
+  },
+  filters: {
+    truncate: function(text, length, suffix) {
+      return text.substring(0, length) + suffix;
+    }
   }
 };
 </script>
@@ -126,8 +141,15 @@ export default {
   max-width: 800px;
 }
 
+.hero-body {
+  background: no-repeat center center;
+  background-size: cover;
+}
+
 .post {
-  font-family: Arial, Avenir;
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", YuGothic,
+    "ヒラギノ角ゴ ProN W3", Hiragino Kaku Gothic ProN, Arial, "メイリオ", Meiryo,
+    sans-serif;
   margin: 1rem 0;
 }
 
@@ -146,7 +168,7 @@ export default {
 }
 
 .media-content {
-  margin-top: 3rem;
+  overflow: hidden;
 }
 
 .article,
@@ -162,6 +184,7 @@ div.column.is-8:first-child {
 .article-subtitle {
   color: #909aa0;
   margin-bottom: 1.5rem;
+  overflow: hidden;
 }
 
 .article-body {
