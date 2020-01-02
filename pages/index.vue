20670ef48547c8be60fcd63e567cd7dbeed70af7
Daiki Nagaoka <neko.z.n1210@gmail.com>
2020-01-02 21:59:21 +0900

    remove import contentful library

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,6 @@
 </template>
 
 <script>
-import contentful from "~/plugins/contentful.js";
 import Particles from "~/components/Particles.vue";
 import Profile from "~/components/Profile.vue";
 import ScrollDown from "~/components/ScrollDown.vue";
@@ -27,7 +26,6 @@ import Skills from "~/components/Skills.vue";
 import Articles from "~/components/Articles.vue";
 import Contacts from "~/components/Contacts.vue";
 
-const client = contentful.createClient();
 export default {
   fetch({ store }) {
     store.commit("resetMenu");
@@ -41,19 +39,6 @@ export default {
     return {
       posts: []
     };
-  },
-  asyncData({ env }) {
-    return client
-      .getEntries({
-        content_type: env.CTF_BLOG_POST_TYPE_ID,
-        order: "-fields.published"
-      })
-      .then(entries => {
-        return {
-          posts: entries.items
-        };
-      })
-      .catch();
   },
   components: { Profile, Skills, ScrollDown, Particles, Articles, Contacts }
 };
